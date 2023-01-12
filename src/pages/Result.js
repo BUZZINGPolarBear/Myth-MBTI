aabcbb0ee2f3c273605958588f73e80ad8ed6832
Jeonjunhowi <jjh63360@kau.kr>
2023-01-13 00:51:14 +0900

    fix: result info style

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,12 +1,29 @@
 import characterResult from "../characterResult";
 import './css/Result.css';
 
+function CharacterInfo(info){
+    const infoList = [];
+
+    for(var property of info.info){
+        infoList.push(<li key={property.id}>{property.content}</li>)
+    }
+    
+
+    return(
+        <div className="InfoList">
+            <ul>
+                {infoList}
+            </ul>  
+        </div>
+    )
+}
 function Result(){
     return(
         <div className="App">
             <div className="TopImageArea">
-                <img src={characterResult[0].imgsrc}></img>
+                <img src={characterResult[0].imgsrc} alt='character img'></img>
             </div>
+            
             <div className="TitleArea">
                 <div className="MainTitle">
                     {characterResult[0].title}
@@ -15,6 +32,13 @@ function Result(){
                     "{characterResult[0].subTitle}"
                 </div>
             </div>
+
+            <div className="InfoArea">
+                <CharacterInfo info={characterResult[0].info} />
+
+            </div>
+
+            
         </div>
         
     )
