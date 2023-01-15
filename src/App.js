1953166587e4f55490b8d6d0075d8f97c4deca36
Jeonjunhowi <jjh63360@kau.kr>
2023-01-15 23:14:40 +0900

    add: browser index page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Result from "./pages/Result";
 import Test from "./pages/TestPage";
-import LandingPage from "./pages/LandingPage";
+import styled from 'styled-components';
 import './App.css';
-import MainHeader from "./pages/MainHeader";
+import {BrowserView, MobileView, isBrowser, isMobile} from "react-device-detect";
 import { useEffect } from "react";
 
 function App() {
@@ -16,18 +16,83 @@ function App() {
     setScreenSize();
   });
 
-  return (
-    <div>
-      <Route exact path="/result">
-        <Result />
-      </Route>
-      <Route exact path="/test">
-        <Test />
-      </Route>
-      {/* <MainHeader /> */}
-      {/* <LandingPage /> */}
-    </div>
-  );
+  if(isBrowser){
+    const BrowserApp = styled.div`
+      width: 60vw;
+      height: 100vh;
+      margin:auto;
+      padding-top: 5vh;
+    `
+    const MainPicArea = styled.div`
+      width: 350px;
+      height: 350px;
+      margin: auto;
+    `
+    const MainPic = styled.img`
+      width: 100%;
+      height: 100%;
+    `
+    const TitleArea = styled.div`
+      width: 100%;
+      font-size: 1.5rem;
+      font-weight: 600;
+
+      margin: auto;
+      margin-top: 1.2rem;
+      
+      text-align: center;
+    `
+
+    const SubTitleArea = styled.div`
+      width: 100%;
+      font-size: 1.2rem;
+
+      margin: auto;
+      margin-top: 1.2rem;
+      line-height: 1.5rem;
+      text-align: center;
+    `
+
+    const StartButton = styled.button`
+      width: 100%;
+      padding: 0.5rem 1rem;
+      margin: auto;
+      margin-top: 2rem;
+
+      border-radius: 20px;
+
+      color: #fff;
+      font-size: 1.2rem;
+      font-weight: 600;
+      cursor: pointer;
+      background-color: #000;
+    `
+    return(
+      <div>
+        
+        <Route exact path="/">
+          <BrowserApp>
+            <MainPicArea>
+              <MainPic src='images/characters/sample.png' alt='main' />
+              <TitleArea>나의 신화 속 사랑 유형 알아보기</TitleArea>
+              <SubTitleArea>신화는 우리의 삶과 맞닿아있습니다. <br></br> 여러분은 어떤 신의 사랑을 하고 있을까요?</SubTitleArea>
+              <StartButton onClick={() => {window.location.href = '/test'}}>시작하기</StartButton>
+            </MainPicArea>
+          </BrowserApp>
+        </Route>
+        
+
+        <Route exact path="/result">
+          <Result />
+        </Route>
+        <Route exact path="/test">
+          <Test />
+        </Route>
+      </div>
+      
+      
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Route} from "react-router-dom";
 // import { Link } from "react-router-dom";
 import './index.css';
 import App from './App';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
+    
   </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
