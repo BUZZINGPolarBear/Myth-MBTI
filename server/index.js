64d80bef51dca0b0ced79dcdf744806a5030c1fc
Jeonjunhowi <jjh63360@kau.kr>
2023-01-16 14:42:58 +0900

    add: express server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+var expressForStatic = require("express");
+const path = require('path');
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// 정적 파일 불러오기
+app.use(expressForStatic.static(path.join(__dirname + "/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 })
 
-app.listen(80)
+app.listen(8080)
